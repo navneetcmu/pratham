10e2bb7ebd6b9b62c8033bfa2373f7097abe58da
Navneet Singh <navneet@cmu.edu>
2017-01-19 01:43:26 -0500

    Add files via upload
    new problem added

---
+++ spoj_nsteps.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class spoj_nsteps{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int count = sc.nextInt();
+for(int i=0; i<count; i++){
+  int x = sc.nextInt();
+  int y = sc.nextInt();
+  if(y>x) System.out.println("No Number");
+  else if(x==y && x%2==0) System.out.println(2*x);
+  else if(x==y && x%2!=0) System.out.println(2*x -1);
+  else System.out.println(x+y - x%2);
+
+}//end for
+} //end main
+} //end class

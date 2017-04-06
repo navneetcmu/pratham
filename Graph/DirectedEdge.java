8142829c253360f88095d1875cb668a11d1bc1dc
Navneet Singh <navneet@cmu.edu>
2017-04-06 19:20:14 -0400

    Create DirectedEdge.java

---
+++ Graph/DirectedEdge.java
@@ -0,0 +1,25 @@
+public class DirectedEdge {
+	
+	private final double weight;
+	private final int v;
+	private final int w;
+	
+	public DirectedEdge(int v, int w, double weight){
+		this.v = v;
+		this.w = w;
+		this.weight = weight; 
+	}
+	
+	public double weight(){
+		return weight;
+	}
+	
+	public int from(){
+		return v;
+	}
+	
+	public int to(){
+		return w; 
+	}
+
+}

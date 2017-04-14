28a34f76afbc729abed8bb0d2b82e914eb488737
Navneet Singh <navneet@cmu.edu>
2017-04-13 20:46:20 -0400

    Update sort.java

--- Strings/Key-indexed counting/sort.java
+++ Strings/Key-indexed counting/sort.java
@@ -1,10 +1,12 @@
 public class sort {
 	
 	kids[] a; 
+	kids[] aux; 
 	
 	public sort(kids[] a, int groups){
 		this.a =a; 
 		int[] count = new int[groups+2]; 
+		aux = new kids[a.length]; 
 		
 		for(int i=0; i<a.length; i++){
 			count[a[i].key()+1]++; 
@@ -14,8 +16,12 @@ public class sort {
 			count[i+1] += count[i]; 
 		}
 		
-		for(int i=0; i<count.length; i++){
-			System.out.println(count[i]);
+		for(int i=0; i<aux.length; i++){
+			aux[count[a[i].key()]++] = a[i];
+		}
+		
+		for(int i=0; i<aux.length; i++){
+			System.out.println(aux[i].name()+" "+aux[i].key());
 		}
 	}
 

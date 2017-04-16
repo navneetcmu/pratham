6b2d8d6a500d0146e7827e7ff02f903d704474d6
Navneet Singh <navneet@cmu.edu>
2017-04-16 19:20:34 -0400

    Update TrieST.java

--- Strings/Tries/TrieST.java
+++ Strings/Tries/TrieST.java
@@ -1,3 +1,5 @@
+import javax.xml.soap.Node;
+
 public class TrieST<Value> {
 	private static int R = 256;
 	private node root;
@@ -8,7 +10,32 @@ public class TrieST<Value> {
 	}
 	
 	public Value get(String key){
-		
+		node x = get(root, key, 0); 
+		if(x==null) return null;
+		return (Value) x.val ; 
 	}
+	
+	private node get(node x, String key, int d){
+		if(x==null) return null;
+		if(d == key.length()) return x;
+		char c = key.charAt(d); 
+		return get(x.next[c], key, d+1); 
+	}
+	
+	public void put(String key, Value val){
+		root = put(root, key, val, 0); 
+	}
+	
+	private node put(node x, String key, Value val, int d){
+		if(x==null) x = new node(); 
+		if(d == key.length()){
+			x.val = val;
+			return x;
+		}
+		char c = key.charAt(d);
+		x.next[c] = put(x.next[c], key, val, d+1);
+		return x; 
+	}
+	
 
 }

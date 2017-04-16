6d369ab60459aebd66e4fe2e8f0add9f5421f78e
Navneet Singh <navneet@cmu.edu>
2017-04-16 18:47:17 -0400

    Create TrieST.java

---
+++ Strings/Tries/TrieST.java
@@ -0,0 +1,14 @@
+public class TrieST<Value> {
+	private static int R = 256;
+	private node root;
+	
+	private static class node{
+		private Object val;
+		private node[] next = new node[R]; 
+	}
+	
+	public Value get(String key){
+		
+	}
+
+}

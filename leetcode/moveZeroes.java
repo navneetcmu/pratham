5b270648f764a432755f578605dc1e3364644c6c
Navneet Singh <navneet@cmu.edu>
2017-04-28 02:49:56 -0400

    Update moveZeroes.java

--- leetcode/moveZeroes.java
+++ leetcode/moveZeroes.java
@@ -1,14 +1,15 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
         int i=0, j=0, zeros=0;
+        
         while(j<nums.length){
             if(nums[j] != 0) nums[i++] = nums[j]; 
             j++;
         }
+        
         while(i<nums.length){
             nums[i] = 0; 
             i++; 
         }
     }
-    
 }

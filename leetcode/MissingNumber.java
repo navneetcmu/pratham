ff02b53545a3c2280bbfcdfe4242da5ec587a0eb
Navneet Singh <navneet@cmu.edu>
2017-05-31 03:16:02 -0400

    added one more method

--- leetcode/MissingNumber.java
+++ leetcode/MissingNumber.java
@@ -13,3 +13,16 @@ public class Solution {
         
     }
 }
+
+//ANOTHER METHOD USING NO EXTRA MEMEORY
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n*(n+1))/2; 
+        for(int i=0; i<n;i++){
+            sum = sum - nums[i];  
+        }
+        return sum; 
+        
+    }
+}
